7496ae46dc06b52e7bf8560d431ace3f08068f14
Ritwik <ritwikb.28@gmail.com>
2019-03-19 17:08:37 +0530

    Interfaces for editing filters' UI completed. Also created the adapters to use for different types of filters.

--- app/src/main/java/com/example/ritwik/photoeditor/Adapter/ThumbnailAdapter.java
+++ app/src/main/java/com/example/ritwik/photoeditor/Adapter/ThumbnailAdapter.java
@@ -1,15 +1,71 @@
 package com.example.ritwik.photoeditor.Adapter;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-/**
- * Created by ritwik on 17-03-2019.
- */
+import com.example.ritwik.photoeditor.FiltersListFragment;
+import com.example.ritwik.photoeditor.Interface.FiltersListFragmentListener;
+import com.example.ritwik.photoeditor.R;
+import com.zomato.photofilters.utils.ThumbnailItem;
+
+import java.util.List;
 
 public class ThumbnailAdapter extends RecyclerView.Adapter<ThumbnailAdapter.MyViewHolder> {
+
+    private List<ThumbnailItem> thumbnailItems;
+    private FiltersListFragmentListener listener;
+    private Context context;
+    private int selectedIndex = 0;
+
+
+    public ThumbnailAdapter(List<ThumbnailItem> thumbnailItems, FiltersListFragmentListener listener, Context context) {
+        this.thumbnailItems = thumbnailItems;
+        this.listener = listener;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(context).inflate(R.layout.thumbnail_item,parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
+        final ThumbnailItem thumbnailItem = thumbnailItems.get(position);
+
+        holder.thumbnail.setImageBitmap(thumbnailItem.image);
+        holder.thumbnail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener.onFilterSelected(thumbnailItem.filter);
+                selectedIndex = position;
+                notifyDataSetChanged();
+
+            }
+        });
+
+        holder.filter_name.setText(thumbnailItem.filterName);
+
+        if(selectedIndex == position) {
+            holder.filter_name.setTextColor(ContextCompat.getColor(context, R.color.selected_filter));
+        } else {
+            holder.filter_name.setTextColor(ContextCompat.getColor(context, R.color.normal_filter));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
         ImageView thumbnail;
@@ -19,6 +75,9 @@ public class ThumbnailAdapter extends RecyclerView.Adapter<ThumbnailAdapter.MyVi
             thumbnail = itemView.findViewById(R.id.thumbnail);
             filter_name = itemView.findViewById(R.id.filter_name);
         }
+
+
+        //I am just writing this so that no one gets suspicious about me sitting till this late and just watching some stuff
 
     }
 }
--- app/src/main/java/com/example/ritwik/photoeditor/Interface/FiltersListFragmentListener.java
+++ app/src/main/java/com/example/ritwik/photoeditor/Interface/FiltersListFragmentListener.java
@@ -2,11 +2,6 @@ package com.example.ritwik.photoeditor.Interface;
 
 import com.zomato.photofilters.imageprocessors.Filter;
 
-/**
- * Created by ritwik on 17-03-2019.
- */
-
 public interface FiltersListFragmentListener {
     void onFilterSelected(Filter filter);
-
 }
